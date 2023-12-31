255951baf82879efc36481d4a045c2c1757ca833
mdaffa48 <work@muhammaddaffa.com>
2023-12-31 11:55:14 +0700

    add TimeParser.java

--- src/main/java/com/muhammaddaffa/mdlib/utils/Common.java
+++ src/main/java/com/muhammaddaffa/mdlib/utils/Common.java
@@ -26,45 +26,6 @@ public class Common {
 
     private static final DecimalFormat decimalFormat = new DecimalFormat("###,###,###,###,###.##");
 
-    public static long parseTime(String string) {
-        // checks if the string is either null and empty and if so returns 0
-        if (string == null || string.isEmpty())
-            return 0L;
-        // this replaces the regex for 0-9 and the other characters
-        string = string.replaceAll("[^0-9smhdw]", "");
-        // checks if the new string is empty since we removed some characters
-        if (string.isEmpty())
-            return 0L;
-        // Check if string contains "w"
-        if (string.contains("w")) {
-            // Replace all non numbers with nothing
-            string = string.replaceAll("[^0-9]", "");
-            // Another empty check
-            if (string.isEmpty())
-                return 0L;
-            // If it has a number we change the number value to days by
-            // multiplying by 7 then we can change it to seconds
-            return TimeUnit.DAYS.toSeconds(Long.parseLong(string) * 7);
-        }
-        // First we check for days using "d"
-        TimeUnit unit = string.contains("d") ? TimeUnit.DAYS
-                // If the string contains "h" it goes for hours
-                : string.contains("h") ? TimeUnit.HOURS
-                // If the string contains "m" it goes for minutes
-                : string.contains("m") ? TimeUnit.MINUTES
-                // Finally, if none match we go with seconds
-                : TimeUnit.SECONDS;
-        // Next we replace all the non-numbers with nothing so it can match a
-        // number
-        string = string.replaceAll("[^0-9]", "");
-        // Another empty check to make sure something is there
-        if (string.isEmpty())
-            return 0L;
-        // Then we return the string as a long in seconds using the unit
-        // selected earlier
-        return unit.toSeconds(Long.parseLong(string));
-    }
-
     public static double getRandomNumberBetween(double min, double max) {
         return ThreadLocalRandom.current().nextDouble(max - min) + min;
     }
