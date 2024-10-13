7d5e35239122f34eca2b615915ce93809b5af564
mdaffa48 <muhammaddaffa912@gmail.com>
2024-10-13 15:14:50 +0700

    2.0.0-beta7

--- src/main/java/com/muhammaddaffa/mdlib/utils/Placeholder.java
+++ src/main/java/com/muhammaddaffa/mdlib/utils/Placeholder.java
@@ -10,22 +10,37 @@ public class Placeholder {
     private final Map<String, String> replacements = new HashMap<>();
 
     public Placeholder add(String original, String replacement) {
+        if (original == null || replacement == null) {
+           return this;
+        }
         this.replacements.put(original, replacement);
         return this;
     }
 
     public Placeholder add(String original, int replacement) {
-        return add(original, replacement + "");
+        return add(original, Integer.toString(replacement));
     }
 
     public Placeholder add(String original, double replacement) {
-        return add(original, replacement + "");
+        return add(original, Double.toString(replacement));
     }
 
     public String translate(String message) {
+        if (message == null) {
+            return null;
+        }
         String translated = message;
+        for (Map.Entry<String, String> entry : this.replacements.entrySet()) {
+            String original = entry.getKey();
+            String replacement = entry.getValue();
+            // Check for null
+            if (original != null && replacement != null) {
+                translated = translated.replace(original, replacement);
+            }
+        }
         for (String original : this.replacements.keySet()) {
-            translated = translated.replace(original, this.replacements.get(original));
+            String result = this.replacements.get(original);
+            if (result != null) translated = translated.replace(original, result);
         }
         return translated;
     }
