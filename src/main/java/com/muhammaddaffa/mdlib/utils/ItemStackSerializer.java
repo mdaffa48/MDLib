517da85284a0b6ce869c993cd20a7c55af736e45
mdaffa48 <muhammaddaffa912@gmail.com>
2024-12-02 15:36:33 +0700

    Add item stack serializer

---
+++ src/main/java/com/muhammaddaffa/mdlib/utils/ItemStackSerializer.java
@@ -0,0 +1,49 @@
+package com.muhammaddaffa.mdlib.utils;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class ItemStackSerializer {
+
+    public static String write(ItemStack... items) {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream)) {
+
+            dataOutput.writeInt(items.length);
+
+            for (ItemStack item : items)
+                dataOutput.writeObject(item);
+
+            return Base64Coder.encodeLines(outputStream.toByteArray());
+
+        } catch (Exception ignored) {
+            return "";
+        }
+    }
+
+    public static ItemStack[] read(String source) {
+        // If the source is null, just return null
+        if (source == null) {
+            return null;
+        }
+
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(source));
+             BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream)) {
+
+            ItemStack[] items = new ItemStack[dataInput.readInt()];
+
+            for (int i = 0; i < items.length; i++)
+                items[i] = (ItemStack) dataInput.readObject();
+
+            return items;
+        } catch (Exception ignored) {
+            return null;
+        }
+    }
+
+}
