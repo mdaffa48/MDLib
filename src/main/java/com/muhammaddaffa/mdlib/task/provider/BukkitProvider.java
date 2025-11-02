b659beda8facb56ae7f5105039144c6a91c15c84
HeadsShot <41201881+Starfruit2210@users.noreply.github.com>
2025-11-03 02:46:51 +0700

    feat: add new ExecutorManager

---
+++ src/main/java/com/muhammaddaffa/mdlib/task/provider/BukkitProvider.java
@@ -0,0 +1,39 @@
+package com.muhammaddaffa.mdlib.task.provider;
+
+import com.muhammaddaffa.mdlib.MDLib;
+import com.muhammaddaffa.mdlib.task.ExecutorProvider;
+import com.muhammaddaffa.mdlib.task.handleTask.HandleTask;
+import org.bukkit.Bukkit;
+
+public class BukkitProvider implements ExecutorProvider {
+
+    @Override
+    public HandleTask sync(Runnable runnable) {
+        return new HandleTask(Bukkit.getScheduler().runTask(MDLib.instance(), runnable));
+    }
+
+    @Override
+    public HandleTask syncLater(long delay, Runnable runnable) {
+        return new HandleTask(Bukkit.getScheduler().runTaskLater(MDLib.instance(), runnable, delay));
+    }
+
+    @Override
+    public HandleTask syncTimer(long delay, long runEvery, Runnable runnable) {
+        return new HandleTask(Bukkit.getScheduler().runTaskTimer(MDLib.instance(), runnable, delay, runEvery));
+    }
+
+    @Override
+    public HandleTask async(Runnable runnable) {
+        return new HandleTask(Bukkit.getScheduler().runTaskAsynchronously(MDLib.instance(), runnable));
+    }
+
+    @Override
+    public HandleTask asyncLater(long delay, Runnable runnable) {
+        return new HandleTask(Bukkit.getScheduler().runTaskLaterAsynchronously(MDLib.instance(), runnable, delay));
+    }
+
+    @Override
+    public HandleTask asyncTimer(long delay, long runEvery, Runnable runnable) {
+        return new HandleTask(Bukkit.getScheduler().runTaskTimerAsynchronously(MDLib.instance(), runnable, delay, runEvery));
+    }
+}
---
+++ src/main/java/com/muhammaddaffa/mdlib/task/provider/FoliaProvider.java
@@ -0,0 +1,85 @@
+package com.muhammaddaffa.mdlib.task.provider;
+
+import com.muhammaddaffa.mdlib.MDLib;
+import com.muhammaddaffa.mdlib.task.ExecutorProvider;
+import com.muhammaddaffa.mdlib.task.handleTask.HandleTask;
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+public class FoliaProvider implements ExecutorProvider {
+
+    @Override
+    public HandleTask sync(Runnable runnable) {
+        return new HandleTask(Bukkit.getGlobalRegionScheduler().run(MDLib.instance(), task -> runnable.run()));
+    }
+
+    @Override
+    public HandleTask syncLater(long delay, Runnable runnable) {
+        return new HandleTask(Bukkit.getGlobalRegionScheduler().runDelayed(MDLib.instance(), task -> runnable.run(), delay));
+    }
+
+    @Override
+    public HandleTask syncTimer(long delay, long runEvery, Runnable runnable) {
+        return new HandleTask(Bukkit.getGlobalRegionScheduler().runAtFixedRate(MDLib.instance(), task -> runnable.run(), delay, runEvery));
+    }
+
+    @Override
+    public HandleTask async(Runnable runnable) {
+        return new HandleTask(Bukkit.getAsyncScheduler().runNow(MDLib.instance(), task -> runnable.run()));
+    }
+
+    @Override
+    public HandleTask asyncLater(long delay, Runnable runnable) {
+        return new HandleTask(Bukkit.getAsyncScheduler().runDelayed(MDLib.instance(), task -> runnable.run(), delay, TimeUnit.SECONDS));
+    }
+
+    @Override
+    public HandleTask asyncTimer(long delay, long runEvery, Runnable runnable) {
+        return new HandleTask(Bukkit.getAsyncScheduler().runAtFixedRate(MDLib.instance(), task -> runnable.run(), delay, runEvery, TimeUnit.SECONDS));
+    }
+
+    // Optional Method
+
+    @Override
+    public HandleTask region(World world, int chunkX, int chunkZ, Runnable runnable) {
+        return new HandleTask(Bukkit.getRegionScheduler().run(MDLib.instance(), world, chunkX, chunkZ, task -> runnable.run()));
+    }
+
+    @Override
+    public HandleTask regionLater(World world, int chunkX, int chunkZ, long delay, Runnable runnable) {
+        return new HandleTask(Bukkit.getRegionScheduler().runDelayed(MDLib.instance(), world, chunkX, chunkZ, task -> runnable.run(), delay));
+    }
+
+    @Override
+    public HandleTask regionTimer(World world, int chunkX, int chunkZ, long delay, long runEvery, Runnable runnable) {
+        return new HandleTask(Bukkit.getRegionScheduler().runAtFixedRate(MDLib.instance(), world, chunkX, chunkZ, task -> runnable.run(), delay, runEvery));
+    }
+
+    @Override
+    public HandleTask entity(Consumer<ScheduledTask> consumer, Runnable runnable) {
+        return new HandleTask(Bukkit.getGlobalRegionScheduler().run(MDLib.instance(), task -> {
+            consumer.accept(task);
+            runnable.run();
+        }));
+    }
+
+    @Override
+    public HandleTask entityLater(Consumer<ScheduledTask> consumer, long delay, Runnable runnable) {
+        return new HandleTask(Bukkit.getGlobalRegionScheduler().runDelayed(MDLib.instance(), task -> {
+            consumer.accept(task);
+            runnable.run();
+        }, delay));
+    }
+
+    @Override
+    public HandleTask entityTimer(Consumer<ScheduledTask> consumer, long delay, long runEvery, Runnable runnable) {
+        return new HandleTask(Bukkit.getGlobalRegionScheduler().runAtFixedRate(MDLib.instance(), task -> {
+            consumer.accept(task);
+            runnable.run();
+        }, delay, runEvery));
+    }
+}
